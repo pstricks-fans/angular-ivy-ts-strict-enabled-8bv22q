2330160aa036c90f678d44c986bed2433b05edea
pstricks-fans <50270635+pstricks-fans@users.noreply.github.com>
2023-02-24 00:03:32 +0700

    1 file modified

--- src/app/datasource.ts
+++ src/app/datasource.ts
@@ -1,26 +1,26 @@
-import { Injectable } from "@angular/core";
-import { Product } from "./product";
+import { Injectable } from '@angular/core';
+import { Product } from './product';
 
 @Injectable()
 export class SimpleDataSource {
-    private data: Product[];
-    private p!: Product;
-    constructor() {
-        this.data = new Array<Product>(
-            new Product(1, "Kayak", "Watersports", 275),
-            new Product(2, "Lifejacket", "Watersports", 48.95),
-            new Product(3, "Soccer Ball", "Soccer", 19.50),
-            new Product(4, "Corner Flags", "Soccer", 34.95),
-            new Product(5, "Thinking Cap", "Chess", 16)
-        );
-        setInterval(() => {
-            this.p = this.data.shift()!;
-            this.data.push(this.p);
-            this.data[0]!.price = this.data[0]!.price! + 1;
-        }, 1000);
-    }
+  private data: Product[];
+  private p!: Product;
+  constructor() {
+    this.data = new Array<Product>(
+      new Product(1, 'Kayak', 'Watersports', 275),
+      new Product(2, 'Lifejacket', 'Watersports', 48.95),
+      new Product(3, 'Soccer Ball', 'Soccer', 19.5),
+      new Product(4, 'Corner Flags', 'Soccer', 34.95),
+      new Product(5, 'Thinking Cap', 'Chess', 16)
+    );
+    setInterval(() => {
+      this.p = this.data.shift()!;
+      this.data.push(this.p);
+      this.data[0]!.price = this.data[0]!.price! + 1;
+    }, 1000);
+  }
 
-    getData(): Product[] {
-        return this.data;
-    }
+  getData(): Product[] {
+    return this.data;
+  }
 }
