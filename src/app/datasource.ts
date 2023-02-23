af6f9a70bc2b941a920e1ac96bfe90c55af8e601
pstricks-fans <50270635+pstricks-fans@users.noreply.github.com>
2023-02-24 00:35:48 +0700

    1 file modified

--- src/app/datasource.ts
+++ src/app/datasource.ts
@@ -15,7 +15,7 @@ export class SimpleDataSource {
     );
     setInterval(() => {
       this.p = this.data.shift()!;
-      this.data.push(p);
+      this.data.push(this.p);
       this.data[0]!.price = this.data[0]!.price! + 1;
     }, 1000);
   }
